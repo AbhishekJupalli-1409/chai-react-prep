013e8c2d1e3ecff53bddcef8d6a4a0c262511eaa
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2026-02-27 00:06:28 +0530

    counter and tailwind

--- MyPractice/01counter/src/App.jsx
+++ MyPractice/01counter/src/App.jsx
@@ -5,29 +5,28 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const Increment = (e)=>{
+    
+    console.log(e);
+     setCount(count + 1);
+     setCount(count + 1);
+     setCount(count + 1); 
+
+  }
+  // var count2 = 0;
+  // const Increment = (e)=>{
+  //   console.log(e);
+  //   count2 = count2 + 1;
+  //   console.log(count2);
+  //   // return setCount(count + 1);
+
+  // }
 
   return (
+    
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>my first counter</h2>
+      <button style={{"borderRadius":"4px","fontSize":"1.5rem","borderBlockColor":"black"}} onClick={Increment}>Counter {count}</button>
     </>
   )
 }
---
+++ MyPractice/02viteproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 >Hello for tailwind js</h2>
+      <h3 className='text-green-500'>hero ser</h3>
+    </>
+  )
+}
+
+export default App
