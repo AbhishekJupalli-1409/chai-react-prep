cbf36d084047e2679d0e3c789ce4f17453478bbc
AbhishekJupalli-1409 <jupalliabhishek1409@gmail.com>
2026-02-28 00:08:58 +0530

    icons todolist

--- MyPractice/04todolist/src/Components/Todo.jsx
+++ MyPractice/04todolist/src/Components/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { deleteIcon, editIcon } from '../assets';
 
 
 function Todo({todos,setTodos}) {
@@ -34,23 +35,23 @@ function Todo({todos,setTodos}) {
               key={todo.id} className='flex flex-col mt-2 w-1/4 max-w-70 text-wrap justify-between min-h-48  rounded-lg h-[3rem]  items-start place-content-between p-2'>
              <div className='w-full flex flex-col gap-3'>
               <div className='flex w-full justify-between'>
-                    <div className='text-gray-800 font-semibold flex gap-2'>
+                    <div className='text-gray-800 font-semibold flex gap-2 px-2 '>
                       <input
                         style={{ transform: "scale(1.5)",accentColor: "#2b2b2ccc" }}
-                       type='checkbox' onChange={()=>handleStrike(todo.id)} className='  border-grey-200 focus:border-grey-500 bg-yellow-600 py-1 px-3 rounded-md text-gray-800 cursor-pointer'></input>
+                       type='checkbox' onChange={()=>handleStrike(todo.id)} className='  border-grey-200 focus:border-grey-500 bg-yellow-600 py-2 px-3 rounded-md text-gray-800 cursor-pointer'></input>
                           {/* {console.log(todo.id)} */}
                     </div>
                     
                     <div className='flex gap-3'>
-                        <button className='bg-yellow-600 py-1 px-3 rounded-md text-gray-800 cursor-pointer' onClick={handleEdit}>
-                          /
+                        <button className='bg-yellow-600 p-1 rounded-full text-gray-800 cursor-pointer' onClick={handleEdit}>
+                          <img src={editIcon} alt="" className='h-4' />
                         </button>
-                        <button className='bg-red-600 py-1 px-3 rounded-md text-gray-800 cursor-pointer' onClick={()=>handleDelete(todo.id)}>
-                          X
+                        <button className=' bg-red-600 px-1  rounded-full text-gray-800 cursor-pointer' onClick={()=>handleDelete(todo.id)}>
+                          <img src={deleteIcon} alt="" className='h-4' />
                         </button>
                     </div>
               </div>
-              <div className="text-lg block text-gray-800 font-semibold text-left" style={{
+              <div className="text-lg block text-gray-800 font-semibold text-left px-2" style={{
                     textDecoration: todo.strike ?"line-through":""
                     }}>{todo.msg}
               </div>
---
+++ MyPractice/04todolist/src/assets/index.js
@@ -0,0 +1,4 @@
+import deleteIcon from './delete-icon.svg';
+import editIcon from './edit-icon.svg';
+
+export {deleteIcon, editIcon};
